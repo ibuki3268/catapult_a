36a55f2c614ef4ff1ddff869c0f480d85432c105
harukamikan <s24b2040@bene.fit.ac.jp>
2025-10-14 19:22:42 +0900

    UI(へっだー)

--- routes/ui.php
+++ routes/ui.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PageController::class, 'welcome']);
-Route::get('/login', [PageController::class, 'login']);
-Route::get('/tasks', [PageController::class, 'taskIndex']);
-Route::get('/tasks/create', [PageController::class, 'taskCreate']);
-Route::get('/tasks/edit', [PageController::class, 'taskEdit']);
-Route::get('/mypage', [PageController::class, 'mypage']);
+Route::get('/', [PageController::class, 'welcome'])->name('welcome');
+Route::get('/login', [PageController::class, 'login'])->name('login');
+Route::get('/tasks', [PageController::class, 'taskIndex'])->name('tasks.index');
+Route::get('/tasks/create', [PageController::class, 'taskCreate'])->name('tasks.create');
+Route::get('/tasks/edit', [PageController::class, 'taskEdit'])->name('tasks.edit');
+Route::get('/mypage', [PageController::class, 'mypage'])->name('mypage');
