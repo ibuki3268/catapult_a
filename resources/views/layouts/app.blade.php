e68480b0906e124dbb21403691a99738ab681410
harukamikan <s24b2040@bene.fit.ac.jp>
2025-10-15 11:55:58 +0900

    ロゴ

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- Tailwind CSS -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-black text-white min-h-screen">
     
