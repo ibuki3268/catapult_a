7cc6de1bad780249389ad203f31267c64f20f63c
ibuki3268 <ryuku3268@gmail.com>
2025-10-18 11:44:17 +0900

    ルート二重定義解消

--- routes/ui.php
+++ routes/ui.php
@@ -32,10 +32,6 @@ Route::get('/tasks/search', [PageController::class, 'taskSearch'])->name('tasks.
 Route::get('/shared/members', [PageController::class, 'sharedMembers'])->name('shared.members');
 
 
-// 完了タスク削除
-Route::get('/tasks/delete-completed', [PageController::class, 'deleteCompletedView'])->name('tasks.deleteCompleted.view');
-Route::delete('/tasks/delete-completed', [PageController::class, 'deleteCompletedExecute'])->name('tasks.deleteCompleted.view');
-
 Route::post('/lists', [ListController::class, 'store'])->name('lists.store');
 Route::put('/lists/{id}', [ListController::class, 'update'])->name('lists.update');
 Route::delete('/lists/{id}', [ListController::class, 'destroy'])->name('lists.destroy');
